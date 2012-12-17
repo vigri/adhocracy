8594853ccba6b004a30987b7caaf583862db0cb2
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2012-12-17 10:24:47 +0100

    All paths are now independent of the adhocracy dir

--- selenium/selenium001.py
+++ selenium/selenium001.py
@@ -12,13 +12,19 @@ import errno
 from check_port_free import check_port_free   
 from selenium import webdriver
 
-
-class vgTest(unittest.TestCase):
+class selTest(unittest.TestCase):
+    # get adhocracy dir
+    adhocracy_dir_arr = os.path.dirname(os.path.abspath(__file__)).split(os.sep)
+    adhocracy_dir_arr_len = len(adhocracy_dir_arr)-3 # remove last 3 elements to ensure we have the 'root' of adhocracy
+    paster_dir_len = len(adhocracy_dir_arr)-4        # paster_interactive.sh is outside the adhocracy dir
+    adhocracy_dir = os.sep+os.path.join(*adhocracy_dir_arr[:adhocracy_dir_arr_len])+os.sep
+    paster_dir = os.sep+os.path.join(*adhocracy_dir_arr[:paster_dir_len])+os.sep
+    
     def check_folder(self,path):
         # Ensure given path exists, if not create it
         try:
             os.makedirs(path)
-	    file = open('bak_db/.git_keep_this', 'w')
+	    file = open(path,'/.git_keep_this', 'w')
 	    file.write('')
 	    file.close()
             
@@ -27,7 +33,7 @@ class vgTest(unittest.TestCase):
                 raise
     def start_selenium_server_standalone(self):
         null=open('/dev/null','wb')
-        cmd = ['java','-jar','res/selenium-2.26.0/selenium-server-standalone-2.26.0.jar']
+        cmd = ['java','-jar',os.path.join(selTest.adhocracy_dir,'src','adhocracy','selenium','res','selenium-2.26.0','selenium-server-standalone-2.26.0.jar')]
         proc = subprocess.Popen(cmd,stderr=null,stdout=null,preexec_fn=os.setsid)
         return proc
     
@@ -39,7 +45,7 @@ class vgTest(unittest.TestCase):
         ## __file__
         ## os.path.dirname(__file__)
         ## os.pazh.join
-        proc = subprocess.Popen('../../../../paster_interactive.sh',stderr=null, stdout=null,preexec_fn=os.setsid)
+        proc = subprocess.Popen(selTest.paster_dir+'paster_interactive.sh',stderr=null, stdout=null,preexec_fn=os.setsid)
         return proc
         
     def shutdown_adhocracy(self, pid):
@@ -58,7 +64,7 @@ class vgTest(unittest.TestCase):
         self.check_folder('bak_db')
         
         # Database isolation - trivial - copy database to some other destination
-        shutil.copyfile('../../../var/development.db','bak_db/adhocracy_backup.db')       
+        shutil.copyfile(os.path.join(selTest.adhocracy_dir,'var','development.db'),os.path.join(selTest.adhocracy_dir,'src','adhocracy','selenium','bak_db','adhocracy_backup.db'))       
     
         # Start Selenium Server Standalone
         self.sel_server = self.start_selenium_server_standalone()
@@ -88,14 +94,14 @@ class vgTest(unittest.TestCase):
         self.shutdown_adhocracy(self.adhocracy.pid)        
 
         # Database isolation - trivial - restore our saved database
-        shutil.copyfile('bak_db/adhocracy_backup.db','../../../var/development.db')
+        shutil.copyfile(os.path.join(selTest.adhocracy_dir,'src','adhocracy','selenium','bak_db','adhocracy_backup.db'),os.path.join(selTest.adhocracy_dir,'var','development.db'))
         
     def test_title_google(self):
         self.driver.get('http://google.com')
         title_tag = self.driver.find_element_by_tag_name('title')
         self.assertEqual(title_tag.text, 'Google')
         
-    def test_title_adhocracy(self):
+    def xtest_title_adhocracy(self):
         self.driver.get('http://adhocracy.lan:5001')
         title_tag = self.driver.find_element_by_tag_name('title')
         #self.assertEqual(title_tag.text, 'Adhocracy')        
