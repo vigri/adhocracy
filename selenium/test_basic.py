c0a7d1dab0bc64906b8a7d4cc3d9ab2f86103c3d
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2013-03-02 22:54:37 +0100

    register rewritten from xpath to css, some errors fixed

--- selenium/test_basic.py
+++ selenium/test_basic.py
@@ -15,6 +15,28 @@ class Test_basic(selTest):
         title_tag = self.driver.find_element_by_tag_name('title')
         self.assertTrue("Adhocracy" in title_tag.text)
 
+    @additionalInfoOnException
+    def test_register(self):
+        self.loadPage()
+        
+        b_register = self.searchAndWait_css('div.register a.button.link_register_now')
+        b_register.click()
+        
+        i_username = self.searchAndWait_css('form[name="create_user"] input[name="user_name"]')
+        i_username.send_keys("selenium_user_test111112")
+        
+        i_email = self.searchAndWait_css('form[name="create_user"] input[name="email"]')
+        i_email.send_keys("selenium_user_test@example1112.com")
+        
+        i_password = self.searchAndWait_css('form[name="create_user"] input[name="password"]')
+        i_password.send_keys("test")
+        
+        i_password2 = self.searchAndWait_css('form[name="create_user"] input[name="password_confirm"]')
+        i_password2.send_keys("test")
+        
+        b_submit = self.searchAndWait_css('form[name="create_user"] input[type="submit"]')
+        b_submit.click()
 
+        self.searchAndWait_css('#user_menu')
 if __name__ == '__main__':
     unittest.main()
