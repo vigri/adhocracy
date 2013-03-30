69bdee32f263a73470ec6ecb58c5be8591027c2c
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2013-03-30 19:50:12 +0100

    .

--- src/adhocracy/selenium/ElementNotFound.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class ElementNotFound(BaseException):
-    def __init__(self, message): #, Errors):
-        BaseException.__init__(self, message)
-
-        #self.Errors = Errors
